981465c73a52c689f0c319420db861b5d360af46
umbruch <seevenup@umbru.ch>
2015-04-12 23:26:36 +0200

    Bugfix: loaded the functions several times

--- pid_status_wp.snippet.php
+++ pid_status_wp.snippet.php
@@ -2,21 +2,25 @@
 //##########################################
 // desc: Diese PHP Script zeig euch ob ein Prozess läuft oder nicht
 // autor: seevenup
-// version: 0.1
+// version: 0.2
 //##########################################
 
-function server_status($string,$name) {
-        $pid=exec("pidof $name");
-        exec("ps -p $pid", $output);
+if (!function_exists('server_status')) {
+        function server_status($string,$name) {
+                $pid=exec("pidof $name");
+                exec("ps -p $pid", $output);
 
-        if (count($output) > 1) {
-                echo "$string: <font color='green'><b>RUNNING</b></font><br>";
-        }
-        else {
-                echo "$string: <font color='red'><b>DOWN</b></font><br>";
+                if (count($output) > 1) {
+                        echo "$string: <font color='green'><b>RUNNING</b></font><br>";
+                }
+                else {
+                        echo "$string: <font color='red'><b>DOWN</b></font><br>";
+                }
         }
 }
 
 //Beispiel "Text zum anzeigen", "Prozess Name auf dem Server"
 server_status("Running With Rifles","rwr_server");
+server_status("Starbound","starbound_server");
+server_status("Minecraft","minecarf");
 ?>
